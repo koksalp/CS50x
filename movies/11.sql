5d196ae2a9e85f42acb20ebf697a91ee48fc65e3
koksalp <72392963+koksalp@users.noreply.github.com>
2022-03-08 19:26:20 +0300

    Add files via upload

---
+++ movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM (SELECT * FROM movies INNER JOIN ratings ON movies.id = ratings.movie_id) WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC LIMIT 5;
