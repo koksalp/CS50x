3e26c0e2751203788c44737a3b90076727d848b7
koksalp <72392963+koksalp@users.noreply.github.com>
2022-03-08 18:39:02 +0300

    Update runoff.c

--- runoff.c
+++ runoff.c
@@ -135,7 +135,7 @@ bool vote(int voter, int rank, string name)
             preferences[voter][rank] = i;
             return true;
         }
-    }
+    } 
     return false;
 }
 // Tabulate votes for non-eliminated candidates
