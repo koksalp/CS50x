f5bba05cd1efb563150d4056596a444f8d099dd0
koksalp <72392963+koksalp@users.noreply.github.com>
2022-03-08 18:39:23 +0300

    Update runoff.c

--- runoff.c
+++ runoff.c
@@ -1,7 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
+#include <string.h> 
+
 // Max voters and candidates
 #define MAX_VOTERS 100
 #define MAX_CANDIDATES 9
